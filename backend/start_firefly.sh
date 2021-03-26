aae9ea97332147b1f7c9bca5686bced213a1846b
Tom Geary <tomgeary@quantexa.com>
2021-03-26 17:25:09 +0000

    exposed port 9001 on app-agent. added in env vars to start script

--- backend/start_firefly.sh
+++ backend/start_firefly.sh
@@ -3,14 +3,19 @@
 # start_firefly.sh
 # control script to launch the firefly app
 
-./start_rmq.sh
-./start_elastic.sh
+#./start_rmq.sh
+#./start_elastic.sh
+#
+#if [[ "$1" = "app-agent"  ]]; then
+#    ./gradlew :app-agent:bootRun &
+#elif [[ "$1" = "app-etl" ]]; then
+#    ./gradlew :app-etl:bootRun &
+#else
+#    ./gradlew :app-agent:bootRun &
+#    ./gradlew :app-etl:bootRun &
+#fi
 
-if [[ "$1" = "app-agent"  ]]; then
-    ./gradlew :app-agent:bootRun &
-elif [[ "$1" = "app-etl" ]]; then
-    ./gradlew :app-etl:bootRun &
-else
-    ./gradlew :app-agent:bootRun &
-    ./gradlew :app-etl:bootRun &
-fi
+export GCP_ACCT_FILE=tg-svc-1.json
+export GCP_ACCT_FILE_DIR=$HOME/src/personal/gcp-bucket-client/
+
+docker-compose up --build &
